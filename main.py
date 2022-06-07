2bd2a8d2a0c52682fa85953c8e6b17437839dd2e
Ramiro Polverini <rfpolverini@bantics.com.ar>
2022-06-07 16:42:58 -0300

    Inicial API

---
+++ main.py
@@ -0,0 +1,102 @@
+import re
+
+from fastapi import FastAPI, HTTPException, Query
+from typing import List
+from typing import Union
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Transposicion(BaseModel):
+    texto: str
+    semitonos: int
+
+
+class Translate(BaseModel):
+    texto: str
+
+@app.post("/transpose/oneNote")
+def transpose_one_note(transposicion: Transposicion):
+    transponer_semitonos = transposicion.semitonos
+    lista_notas_do = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+    transposicion.texto = transposicion.texto.upper()
+    try:
+        posicion = lista_notas_do.index(transposicion.texto)
+        posicion_transp = posicion + transponer_semitonos
+        if posicion_transp > 11:
+            posicion_transp = posicion_transp - 12
+        return {"entro": transposicion.texto, "transp": lista_notas_do[posicion_transp]}
+
+    except ValueError:
+        raise HTTPException(status_code=404, detail="a este recurso deberias enviar solamente de a una nota en cifrado americano (ejemplo DO=C Fa# = F#")
+
+# C a Bb son 2 semitonos--- C a Eb son 9 semitonos
+# Eb a Bb son 5 semitonos- de alto a tenor
+@app.post("/transpose/string/")
+def transpose_string(transposicion: Transposicion):
+    transposicion.texto = transposicion.texto.upper()
+    notas_a_transponer = re.sub(' +', ' ', transposicion.texto)
+    notas_a_transponer = notas_a_transponer.split(" ")
+
+    transponer_semitonos = transposicion.semitonos
+    lista_notas_do = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+
+    lista_transpuesta = []
+    for nota in notas_a_transponer:
+        try:
+            posicion = lista_notas_do.index(nota)
+            posicion_transp = posicion + transponer_semitonos
+            if posicion_transp > 11:
+                posicion_transp = posicion_transp - 12
+            lista_transpuesta.append(lista_notas_do[posicion_transp])
+        except ValueError:
+            raise HTTPException(status_code=404, detail="Alguna No es una nota correcta che...")
+
+    return {"entro": transposicion.texto, "transp": lista_transpuesta}
+
+@app.post("/translate/popularToAmericano/")
+def translate_song_doremi_cde(translate: Translate):
+    translate.texto = translate.texto.upper()
+    notas_a_transponer = re.sub(' +', ' ', translate.texto)
+    notas_a_transponer = notas_a_transponer.split(" ")
+
+    lista_notas_do = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+    lista_notas_Popular = ["DO", "DO#", "RE", "RE#", "MI", "FA", "FA#", "SOL", "SOL#", "LA", "LA#", "SI"]
+
+    lista_transpuesta = []
+    for nota in notas_a_transponer:
+        try:
+            posicion = lista_notas_Popular.index(nota)
+            posicion_transp = posicion
+            if posicion_transp > 11:
+                posicion_transp = posicion_transp - 12
+            lista_transpuesta.append(lista_notas_do[posicion_transp])
+        except ValueError:
+            raise HTTPException(status_code=404, detail="Alguna No es una nota correcta che...")
+
+    return {"entro": translate.texto, "transp": lista_transpuesta}
+
+
+@app.post("/translate/americanoToPopular/")
+def translate_song_cde_doremi(translate: Translate):
+    translate.texto = translate.texto.upper()
+    notas_a_transponer = re.sub(' +', ' ', translate.texto)
+    notas_a_transponer = notas_a_transponer.split(" ")
+
+    lista_notas_do = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+    lista_notas_Popular = ["DO", "DO#", "RE", "RE#", "MI", "FA", "FA#", "SOL", "SOL#", "LA", "LA#", "SI"]
+
+    lista_transpuesta = []
+    for nota in notas_a_transponer:
+        try:
+            posicion = lista_notas_do.index(nota)
+            posicion_transp = posicion
+            if posicion_transp > 11:
+                posicion_transp = posicion_transp - 12
+            lista_transpuesta.append(lista_notas_Popular[posicion_transp])
+        except ValueError:
+            raise HTTPException(status_code=404, detail="Alguna No es una nota correcta che...")
+
+    return {"entro": translate.texto, "transp": lista_transpuesta}
+
---
+++ readme.md
@@ -0,0 +1,79 @@
+<p align="center">
+  <img src="NotesLogo.png" alt="NotesAPI">
+</p>
+<p align="center">
+    <em>FastNotes, musical notes transposition Api, specially for saxophonists but free for all friendly musicians :D</em>
+</p>
+
+
+# Api para transposición de notas musicales
+
+En principio la idea es tener algunos endpoints que permitan transponer notas musicales de a una, y en "conjunto", para poder pasar los arreglos que tenia para saxofon Alto, para poder practicar con el Soprano y Tenor.
+
+# Lanzar localmente
+ Se recomienda crear entorno limpio de python3 con virtualenv 
+
+ git clone https://github.com/rpolverini/fastNotes.git
+
+ pip install -r requirements.txt 
+
+ uvicorn main:app
+
+
+# Documentación
+Podes verificar la documentación en swagger, gracias a FastAPI 
+http://127.0.0.1:8000/docs
+
+
+#### Roadmap / Todo List
+- Deploy en docker container u otra forma serverless.
+- Realizar tests unitarios
+
+
+Al enviar un objeto json con los semitonos a transponer, con esta herramienta los músicos podrán (entre otras):
+
+
+* **De Piano (Do/C) a saxo Tenor/Soprano **: Transponer de Tonalidad C a Bb son 2 semitonos ("/transpose/string/") 
+
+``` 
+ {"semitonos": 2,
+ "texto": "a"}
+``` 
+
+* **De Piano (Do/C) a saxo Alto **: Transponer de Tonalidad C a Eb son 9 semitonos ("/transpose/string/")
+
+``` 
+ {"semitonos": 9,
+ "texto": "a"}
+``` 
+
+* **De Saxo Alto a saxo Tenor/Soprano  **: Transponer de Tonalidad Eb a Bb son 5 semitonos- de alto a tenor ("/transpose/string/")
+``` 
+ {"semitonos": 5,
+ "texto": "a"}
+``` 
+
+* **Traducir**: de cifrado tradicional a cifrado Americano ("/translate/popularToAmericano/"):
+``` 
+{
+ "texto": "LA SI DO"
+}
+``` 
+
+
+* **Traducir**: de cifrado Americano a cifrado tradicional ("/translate/americanoToPopular/"):
+
+``` 
+{
+ "texto": "C D f#"
+}
+``` 
+
+Pueden verificar cada endpoint gracias a la documentación automatica de FastAPI en localhost/docs
+
+
+Ver info adicional
+
+https://cdn.shopify.com/s/files/1/1069/0122/files/NOMBRE-DE-ACORDES-ESPANOL-E-INGLES-II_large.jpg
+
+https://thevisualsoundmethod.com/wp-content/uploads/2016/02/tabla-de-transposicion-do-mi-bemol-si-bemol.png
